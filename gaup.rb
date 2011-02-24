d11ec4dc312355f6dc73dc8502623c2d6e3ff03f
Oddmund Strømme <oddmund@oddmundo.com>
2011-02-24 02:55:28 +0100

    back

--- gaup.rb
+++ gaup.rb
@@ -8,6 +8,6 @@ get %r{/w ([\w]+)} do |a|
   redirect "http://en.wikipedia.org/wiki/#{a}"
 end
 
-#get ':q' do
-#  redirect "http://google.com/search?q=#{params[:q]}"
-#end
+get ':q' do
+  redirect "http://google.com/search?q=#{params[:q]}"
+end
