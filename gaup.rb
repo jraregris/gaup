0e5c331000ce727def1b70ffc969e97dee993daf
Oddmund Strømme <oddmund@oddmundo.com>
2011-02-24 02:36:22 +0100

    wikki

--- gaup.rb
+++ gaup.rb
@@ -4,7 +4,9 @@ get '/thesusanne' do
   redirect 'http://thesusanne.com'
 end
 
-
+get 'w :q' do
+  redirect "http://en.wikipedia.org/wiki/#{params[:q]}"
+end
 
 get '/:q' do
   redirect "http://google.com/search?q=#{params[:q]}"
